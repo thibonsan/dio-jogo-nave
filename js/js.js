6f7c5ec001711022a7a9b1fb932594928d3b0417
Thiago Bonetti dos Santos <thibonsan@gmail.com>
2022-12-10 17:55:01 +0000

    Parte 8.

--- js/js.js
+++ js/js.js
@@ -6,15 +6,53 @@ function start() {
 	$("#fundoGame").append("<div id='amigo' class='anima3'></div>");
 
     var jogo = {}
+
+    var TECLA = {
+        W: 87,
+        S: 83,
+        D: 68
+    }
+
+    // Identificação das teclas pressionadas
+    
+    jogo.pressionou = [];
+
+    $(document).keydown(function(e) {
+        jogo.pressionou[e.which] = true;
+    });
+
+    $(document).keyup(function(e) {
+        jogo.pressionou[e.which] = false;
+    });
+
+    // Game loop
 	
 	jogo.timer = setInterval(loop, 30);
 	
 	function loop() {	
-	    movefundo();	
+	    movefundo();
+        movejogador();	
 	}
 
     function movefundo() {	
         esquerda = parseInt($("#fundoGame").css("background-position"));
         $("#fundoGame").css("background-position", esquerda - 1);
+    }
+
+    function movejogador() {
+	
+        if (jogo.pressionou[TECLA.W]) {
+            var topo = parseInt($("#jogador").css("top"));
+            $("#jogador").css("top", topo - 10);        
+        }
+        
+        if (jogo.pressionou[TECLA.S]) {            
+            var topo = parseInt($("#jogador").css("top"));
+            $("#jogador").css("top",topo + 10);	
+        }
+        
+        if (jogo.pressionou[TECLA.D]) {            
+            //Chama função Disparo	
+        }
     }
 }
