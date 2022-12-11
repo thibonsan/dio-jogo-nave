83fc12f1777f1b0f8139b5f7a54c7f1fd78fce8c
Thiago Bonetti dos Santos <thibonsan@gmail.com>
2022-12-11 17:07:59 +0000

    Parte 16.

--- js/js.js
+++ js/js.js
@@ -183,6 +183,15 @@ function start() {
             reposicionaAmigo();
             $("#amigo").remove();
         }
+
+        // Inimigo2 com o amigo		
+        if (colisao6.length > 0) {                
+            amigoX = parseInt($("#amigo").css("left"));
+            amigoY = parseInt($("#amigo").css("top"));
+            explosao3(amigoX, amigoY);
+            $("#amigo").remove();                    
+            reposicionaAmigo();                    
+        }
     }
 
     function explosao1(inimigo1X,inimigo1Y) {
@@ -241,5 +250,18 @@ function start() {
                 $("#fundoGame").append("<div id='amigo' class='anima3'></div>");            
             }            
         }        
-    } 
+    }
+
+    function explosao3(amigoX, amigoY) {
+        $("#fundoGame").append("<div id='explosao3' class='anima4'></div");
+        $("#explosao3").css("top", amigoY);
+        $("#explosao3").css("left", amigoX);
+        var tempoExplosao3 = window.setInterval(resetaExplosao3, 1000);
+
+        function resetaExplosao3() {
+            $("#explosao3").remove();
+            window.clearInterval(tempoExplosao3);
+            tempoExplosao3 = null;
+        }        
+    }
 }
